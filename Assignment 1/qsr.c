c727d7ec441354abefac532f43bea0da2e84cc7c
Madsmal <Madsmalmskov@hotmail.com>
2017-09-26 22:28:26 +0200

    Update qsr.c

--- Assignment 1/qsr.c
+++ Assignment 1/qsr.c
@@ -1,8 +1,86 @@
 #include "qsr.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-void peakDetection(QRS_params *params)
-{
+int peaksTime[24] = { 0 };
+int peaksValue[24] = { 0 };
+int RpeakTime[8] = { 0 };
+int RpeakValue[8] = { 0 };
+int counter = 0;
+int counter1 = 0;
+int threshold1 = 0;
+int threshold2 = 0;
+int NPKF = 0;
+int SPKF = 2500;
+int RR = 0;
 
+void peakDetection() {
+	if (mwiBuffY[index - 1] < mwiBuffY[index] > mwiBuffY[index + 1]) {
+		peaksValue[counter] = mwiBuffY[index];
+		peaksTime[counter] = clock();
+	}
+
+
+	RR = (peaksTime[counter] + peaksTime[counter - 1]) / 2;
+// if (peaksValue[counter] > threshold1) {
+// }
+	if (peaksValue[counter] < threshold1) {
+			NPKF = peaksValue[counter] / 8 + (7 * NPKF) / 8;
+			threshold1 = NPKF + (SPKF - NPKF) / 4;
+			threshold2 = threshold1 / 2;
+
+	}
+
+	int sum = 0;
+	for (int i = 7;i>=1;i--){
+		sum = sum + (peaksTime[i]-peaksTime[i-1]);
+	}
+	int RRaverage1 =sum/8;
+	int Rsum = 0;
+	for (int i = 7;i>=1;i--){
+			Rsum = Rsum + (RpeakTime[i]-RpeakTime[i-1]);
+		}
+
+
+	int RRaverage2 = 0;
+	int RR_low = 0;
+	int RR_high = 0;
+	int RR_miss = 0;
+
+	if (RR_low < RR && RR < RR_high) {
+			RpeakValue[counter] = peaksValue[counter];
+			RpeakTime[counter] = peaksTime[counter];
+			SPKF = peaksValue[counter]/8 + (7*SPKF)/8;
+			RRaverage2 = Rsum/8 ;
+			RR_low = RRaverage2 * 0.92;
+			RR_high = RRaverage2 * 1.16;
+			RR_miss = RRaverage2 * 1.66;
+			threshold1 = NPKF + (SPKF - NPKF) / 4;
+			threshold2 = threshold1 / 2;
+
+
+	}
+	else if (RR > RR_miss){
+		// Implement Searchback here
+		for (int i = counter;i>=counter-7;i--){
+			if (peaksValue[i]>threshold2){
+
+			}
+		}
+
+	}
+	if (counter < 23) {
+		counter++;
+
+	} else if (counter == 23) {
+		counter = 0;
+	}
+
+	if (counter1 < 7) {
+		counter1++;
+
+	} else if (counter1 == 7) {
+		counter1 = 0;
+	}
 }
