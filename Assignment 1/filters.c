c6698ffeed6fadaf0b135102a822a032fa719117
Madsmal <Madsmalmskov@hotmail.com>
2017-09-27 16:35:32 +0200

    Update filters.c

--- Assignment 1/filters.c
+++ Assignment 1/filters.c
@@ -8,14 +8,13 @@ int highBuffX[32] = {0};
 int deriBuffX[4] = {0};
 
 int mwiBuffX[30] = {0};
-int mwiBuffY[3] = {0};
-// Feel free to change return statement and arguments
+
 int lowPassFilter(int x)
 {
 
 
 	int yn;
-	//printf("%d\n",x);
+
 	yn = 2*lowBuffY[0] - lowBuffY[1]	 + ((x-(2*lowBuffX[5])+lowBuffX[11])/32);
 
 
@@ -27,37 +26,27 @@ int lowPassFilter(int x)
 
 	lowBuffY[1] = lowBuffY[0];
 	lowBuffY[0] = yn;
-	// printf("%d\n",yn);
-	// for (int i = 0;i<12;i++){
-	// printf("%d,",lowBuffX[i]);
-	// }
-	// printf("\n");
+
 	return yn;
 
-	// i = i+1;
-	// yBufferArr[i] = 2*yBufferArr[((i-1)+11)%12] - yBufferArr[(i-2+11)%12] + (1/32)*(xBufferArr[(i+11)%12]-
-	//		2*xBufferArr[(i-6+11)%12<]+xBufferArr[(i-12+11)%12]);
-	// return 2*yBufferArr[((i-1)+11)%12] - yBufferArr[(i-2+11)%12] + (1/32)*(xBufferArr[(i+11)%12]-
-	//			2*xBufferArr[(i-6+11)%12]+xBufferArr[(i-12+11)%12]);
 
 	}
 int highPassFilter(int x) {
 
 	int yn;
 
-// printf("x val %d \n", x);
+
 	yn = highBuffY - (x/32) + highBuffX[15] - highBuffX[16] + (highBuffX[31]/32);
-	// printf("%d,%d,%d,%d,%d\n",highBuffY,x,highBuffX[15],highBuffX[16],highBuffX[31]);
-	// printf("%d\n",yn);
+
 	for (int i = 31; i >= 1; i--){
 		highBuffX[i] = highBuffX[i-1];
 	}
 	highBuffX[0] = x;
 	highBuffY = yn;
 	for (int i = 0;i<12;i++){
-		//printf("%d,",lowBuffX[i]);
+
 		 }
-		 //printf("\n");
+
 	return yn;
 }
 
@@ -71,7 +60,7 @@ int derivativeFilter(int x) {
 			deriBuffX[i] =deriBuffX[i-1];
 		}
 		deriBuffX[0] = x;
-		// printf("%d\n",yn);
+
 		return yn;
 }
 
@@ -85,7 +74,6 @@ int mwiPass(int x){
 	int N = 30;
 	int sum = x;
 
-
 	for (int i = 0; i < 30; i++){
 		sum = sum + mwiBuffX[i];
 
@@ -94,13 +82,6 @@ int mwiPass(int x){
 					mwiBuffX[i] = mwiBuffX[i-1];
 	}
 
-	mwiBuffX[0]=x;
-	for(int i=2;i>=1;i--){
-		mwiBuffY[i]=mwiBuffY[i+1];
-	}
-	mwiBuffY[0]=sum/N;
-
-	// printf("%d\n",sum/N);
 	return sum/N;
 
 }
