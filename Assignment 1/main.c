86c8c10a5f240581aae64c84d18341940799d710
Madsmal <Madsmalmskov@hotmail.com>
2017-10-02 15:03:56 +0200

    Update main.c

--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 
 
-	int yBufferArr[12]={0};
-	int xBufferArr[12]={0};
-	int myIndex = 0;
-	int timeCounter = 0;
-	int timeInMiliSec = 0;
-	int finalData[3];
+int yBufferArr[12]={0};
+int xBufferArr[12]={0};
+int myIndex = 0;
+int timeCounter = 0;
+float timeInMiliSec = 0;
+int finalData[3];
 
 // Main function for organizing the program execution.
 // The functions and object predefined are just for inspiration.
@@ -19,7 +19,7 @@
 int main()
 {	
 
-  //  QRS_params qsr_params;       // Instance of the made avaiable through: #include "qsr.h"
+	//  QRS_params qsr_params;       // Instance of the made avaiable through: #include "qsr.h"
 	FILE *file;                  // Pointer to a file object
 	file = openfile("ECG.txt");
 
@@ -31,37 +31,38 @@ int main()
 	finalData[3] = 0;
 
 
-    //getNextData(file);          // Read Data from Sensor
-    while (!feof(file)){
-    	x = getNextData(file);
-    	lowPassY = lowPassFilter(x);
-    	// printf("%d\n",lowPassY);
-    	highPassY = highPassFilter(lowPassY);
-    	derivY = derivativeFilter(highPassY);
-    	squareY = squarePass(derivY);
-    	// printf("Reads first while \n");
+	//getNextData(file);          // Read Data from Sensor
+	while (!feof(file)){
+		x = getNextData(file);
+		lowPassY = lowPassFilter(x);
+		// printf("%d\n",lowPassY);
+		highPassY = highPassFilter(lowPassY);
+		derivY = derivativeFilter(highPassY);
+		squareY = squarePass(derivY);
+		// printf("Reads first while \n");
 
-    		for(int i=2;i>0;i--){
-    			finalData[i]=finalData[i-1];
+		for(int i=2;i>0;i--){
+			finalData[i]=finalData[i-1];
 
-    		}
-    		finalData[0]= mwiPass(squareY);
+		}
+		finalData[0]= mwiPass(squareY);
 
-    	// while (myIndex < 3){
-    	if (myIndex >= 3) {
-    	peakDetection(myIndex, finalData, timeInMiliSec);
-    		// printf("%d\n",finalData[myIndex]);
+		// while (myIndex < 3){
+		if (myIndex >= 3) {
+			peakDetection(finalData, timeInMiliSec);
+			// printf("%d\n",finalData[myIndex]);
 
-    	}
+		}
 
-    	myIndex++;
-    	// }
-    	// printf("Done with while 2 %d\n");
+		myIndex++;
+		// }
+		// printf("Done with while 2 %d\n");
 
-    	timeCounter++;
-    	timeInMiliSec = timeCounter/2.5;
+		timeCounter++;
+		timeInMiliSec = timeCounter/2.5;
 
-    }
+
+	}
 
 
 
