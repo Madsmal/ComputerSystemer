f88432c9b8d7dee02e5dc8c66b541e64c5f770c8
Madsmal <Madsmalmskov@hotmail.com>
2017-10-03 20:18:29 +0200

    Update qsr.h

--- Assignment 1/qsr.h
+++ Assignment 1/qsr.h
@@ -1,22 +1,17 @@
 #ifndef QSR_H
 #define QSR_H
 
-// Header file for QRS functionality 
-// it is recommended to use the structure "QRS_parameters"
-// to organize all variables and parameters
 
-typedef struct QRS_params
-{ // Structure for QRS parameters
-   int SPKF;
-   int NPKF; 
-   int THRESHOLD1;
-   int THRESHOLD2;
-   // Add parameters that are missing
-   // or of use to you
-   
+typedef struct
+{
+	int SPKF;
+	int NPKF;
+	int THRESHOLD1;
+
+
 } QRS_params;
 
-// Feel free to change and add methods
-void peakDetection(int myIndex,int *mwiBuffY, int timeInMiliSec);
 
-#endif // QSR_H
+void peakDetection(int *mwiBuffY, int timeInMiliSec);
+
+#endif
